6d6a3173e8b052a13ffd9b1464fe5f4ade2e33ba
DRed96 <roig_96@hotmail.com>
2015-05-16 11:53:45 +0200

    TODO

--- ModulePlayer2.cpp
+++ ModulePlayer2.cpp
@@ -92,7 +92,9 @@ Uint32 jump_timer = 0;
 Uint32 punch_timer = 0;
 Uint32 hit_timer = 0;
 
-bool ModulePlayer2::external_input(p2Qeue<p2_inputs>& inputs)
+
+//TODO SINGLE STATE MACHINE
+bool ModulePlayer2::external_input(p2Qeue<p1_inputs>& inputs)
 {
 	static bool left = false;
 	static bool right = false;
@@ -111,22 +113,22 @@ bool ModulePlayer2::external_input(p2Qeue<p2_inputs>& inputs)
 			case SDLK_ESCAPE:
 				return false;
 				break;
+			case SDLK_DOWN:
+				inputs.Push(IN_CROUCH_UP);
+				down = false;
+				break;
 			case SDLK_KP_1:
 				punch_l = false;
-				break;
-			case SDLK_DOWN:
-				inputs.Push(_2_IN_CROUCH_UP);
-				down = false;
 				break;
 			case SDLK_UP:
 				up = false;
 				break;
 			case SDLK_LEFT:
-				inputs.Push(_2_IN_LEFT_UP);
+				inputs.Push(IN_LEFT_UP);
 				left = false;
 				break;
 			case SDLK_RIGHT:
-				inputs.Push(_2_IN_RIGHT_UP);
+				inputs.Push(IN_RIGHT_UP);
 				right = false;
 				break;
 			}
@@ -135,11 +137,11 @@ bool ModulePlayer2::external_input(p2Qeue<p2_inputs>& inputs)
 		{
 			switch (event.key.keysym.sym)
 			{
-			case SDLK_KP_1:
-				punch_l = false;
+			case SDLK_q:
+				punch_l = true;
 				break;
-			case SDLK_j:
-				inputs.Push(_2_IN_H);
+			case SDLK_h:
+				inputs.Push(IN_H);
 				break;
 			case SDLK_UP:
 				up = true;
@@ -158,38 +160,37 @@ bool ModulePlayer2::external_input(p2Qeue<p2_inputs>& inputs)
 	}
 
 	if (left && right)
-		inputs.Push(_2_IN_LEFT_AND_RIGHT);
+		inputs.Push(IN_LEFT_AND_RIGHT);
 	{
 		if (left)
-			inputs.Push(_2_IN_LEFT_DOWN);
+			inputs.Push(IN_LEFT_DOWN);
 		if (right)
-			inputs.Push(_2_IN_RIGHT_DOWN);
+			inputs.Push(IN_RIGHT_DOWN);
 	}
 
 	if (up && down)
-		inputs.Push(_2_IN_JUMP_AND_CROUCH);
+		inputs.Push(IN_JUMP_AND_CROUCH);
 	else
 	{
 		if (down)
-			inputs.Push(_2_IN_CROUCH_DOWN);
+			inputs.Push(IN_CROUCH_DOWN);
 		if (up)
-			inputs.Push(_2_IN_JUMP);
+			inputs.Push(IN_JUMP);
 	}
-
 	if (punch_l)
 	{
-		inputs.Push(_2_IN_X);
+		inputs.Push(IN_X);
 	}
 	return true;
 }
 
-void ModulePlayer2::internal_input(p2Qeue<p2_inputs>& inputs)
+void ModulePlayer2::internal_input(p2Qeue<p1_inputs>& inputs)
 {
 	if (jump_timer > 0)
 	{
 		if (SDL_GetTicks() - jump_timer > JUMP_TIME)
 		{
-			inputs.Push(_2_IN_JUMP_FINISH);
+			inputs.Push(IN_JUMP_FINISH);
 			jump_timer = 0;
 		}
 	}
@@ -197,8 +198,9 @@ void ModulePlayer2::internal_input(p2Qeue<p2_inputs>& inputs)
 	if (punch_timer > 0)
 	{
 		if (SDL_GetTicks() - punch_timer > PUNCH_TIME)
+			//	if(current_animation->getFrame() >= current_animation->frames.Count() - current_animation->speed)
 		{
-			inputs.Push(_2_IN_PUNCH_FINISH);
+			inputs.Push(IN_PUNCH_FINISH);
 			punch_timer = 0;
 		}
 	}
@@ -207,155 +209,165 @@ void ModulePlayer2::internal_input(p2Qeue<p2_inputs>& inputs)
 	{
 		if (SDL_GetTicks() - hit_timer > HIT_TIME)
 		{
-			inputs.Push(_2_IN_HIT_FINISH);
+			inputs.Push(IN_HIT_FINISH);
 			punch_timer = 0;
 		}
 	}
 }
 
-p2_states ModulePlayer2::process_fsm(p2Qeue<p2_inputs>& inputs)
+p1_states ModulePlayer2::process_fsm(p2Qeue<p1_inputs>& inputs)
 {
-	static p2_states state = _2_ST_IDLE;
-	p2_inputs last_input;
-	int speed = 3;
+	static p1_states state = ST_IDLE;
+	p1_inputs last_input;
 
 	while (inputs.Pop(last_input))
 	{
 		switch (state)
 		{
-		case _2_ST_IDLE:
+		case ST_IDLE:
 		{
 			switch (last_input)
 			{
-			case _2_IN_RIGHT_DOWN: state = _2_ST_WALK_FORWARD; break;
-			case _2_IN_LEFT_DOWN: state = _2_ST_WALK_BACKWARD; break;
-			case _2_IN_JUMP: state = _2_ST_JUMP_NEUTRAL; jump_timer = SDL_GetTicks();  break;
-			case _2_IN_CROUCH_DOWN: state = _2_ST_CROUCH; break;
-			case _2_IN_X: state = _2_ST_PUNCH_STANDING_L; punch_timer = SDL_GetTicks();  break;
-			case _2_IN_H: state = _2_ST_HIT; hit_timer = SDL_GetTicks();  break;
+			case IN_RIGHT_DOWN: state = ST_WALK_FORWARD; break;
+			case IN_LEFT_DOWN: state = ST_WALK_BACKWARD; break;
+			case IN_JUMP: state = ST_JUMP_NEUTRAL; jump_timer = SDL_GetTicks();  break;
+			case IN_CROUCH_DOWN: state = ST_CROUCH; break;
+			case IN_X:
+			{
+				if (isOnLeft){
+					c_punch1 = App->colision->AddCollider({ position.x + 10, position.y - 75, 50, 10 }, COLLIDER_PUNCH_1, this);
+				}
+				else{
+					c_punch1 = App->colision->AddCollider({ position.x - 60, position.y - 75, 50, 10 }, COLLIDER_PUNCH_1, this);
+				}
+				punch_timer = SDL_GetTicks();
+				state = ST_PUNCH_STANDING_L;
+			}
+			break;
+			case IN_H: state = ST_HIT; hit_timer = SDL_GetTicks();  break;
 			}
 		}
 		break;
 
-		case _2_ST_HIT:
+		case ST_HIT:
 		{
 			switch (last_input)
 			{
-			case _2_IN_HIT_FINISH: state = _2_ST_IDLE; break;
+			case IN_HIT_FINISH: state = ST_IDLE; break;
 			}
 		}
 		break;
 
 
-		case _2_ST_WALK_FORWARD:
+		case ST_WALK_FORWARD:
 		{
 			switch (last_input)
 			{
-			case _2_IN_RIGHT_UP: state = _2_ST_IDLE; break;
-			case _2_IN_LEFT_AND_RIGHT: state = _2_ST_IDLE; break;
-			case _2_IN_JUMP: state = _2_ST_JUMP_FORWARD; jump_timer = SDL_GetTicks();  break;
-			case _2_IN_CROUCH_DOWN: state = _2_ST_CROUCH; break;
+			case IN_RIGHT_UP: state = ST_IDLE; break;
+			case IN_LEFT_AND_RIGHT: state = ST_IDLE; break;
+			case IN_JUMP: state = ST_JUMP_FORWARD; jump_timer = SDL_GetTicks();  break;
+			case IN_CROUCH_DOWN: state = ST_CROUCH; break;
 			}
 		}
 		break;
 
-		case _2_ST_WALK_BACKWARD:
+		case ST_WALK_BACKWARD:
 		{
 			switch (last_input)
 			{
-			case _2_IN_LEFT_UP: state = _2_ST_IDLE; break;
-			case _2_IN_LEFT_AND_RIGHT: state = _2_ST_IDLE; break;
-			case _2_IN_JUMP: state = _2_ST_JUMP_BACKWARD; jump_timer = SDL_GetTicks();  break;
-			case _2_IN_CROUCH_DOWN: state = _2_ST_CROUCH; break;
+			case IN_LEFT_UP: state = ST_IDLE; break;
+			case IN_LEFT_AND_RIGHT: state = ST_IDLE; break;
+			case IN_JUMP: state = ST_JUMP_BACKWARD; jump_timer = SDL_GetTicks();  break;
+			case IN_CROUCH_DOWN: state = ST_CROUCH; break;
 			}
 		}
 		break;
 
-		case _2_ST_JUMP_NEUTRAL:
+		case ST_JUMP_NEUTRAL:
 		{
 			switch (last_input)
 			{
-			case _2_IN_JUMP_FINISH: state = _2_ST_IDLE; break;
-			case _2_IN_X: state = _2_ST_PUNCH_NEUTRAL_JUMP; punch_timer = SDL_GetTicks(); break;
+			case IN_JUMP_FINISH: state = ST_IDLE; break;
+			case IN_X: state = ST_PUNCH_NEUTRAL_JUMP; punch_timer = SDL_GetTicks(); break;
 			}
 		}
 		break;
 
-		case _2_ST_JUMP_FORWARD:
+		case ST_JUMP_FORWARD:
 		{
 			switch (last_input)
 			{
-			case _2_IN_JUMP_FINISH: state = _2_ST_IDLE; break;
-			case _2_IN_X: state = _2_ST_PUNCH_FORWARD_JUMP; punch_timer = SDL_GetTicks(); break;
+			case IN_JUMP_FINISH: state = ST_IDLE; break;
+			case IN_X: state = ST_PUNCH_FORWARD_JUMP; punch_timer = SDL_GetTicks(); break;
 			}
 		}
 		break;
 
-		case _2_ST_JUMP_BACKWARD:
+		case ST_JUMP_BACKWARD:
 		{
 			switch (last_input)
 			{
-			case _2_IN_JUMP_FINISH: state = _2_ST_IDLE; break;
-			case _2_IN_X: state = _2_ST_PUNCH_BACKWARD_JUMP; punch_timer = SDL_GetTicks(); break;
+			case IN_JUMP_FINISH: state = ST_IDLE; break;
+			case IN_X: state = ST_PUNCH_BACKWARD_JUMP; punch_timer = SDL_GetTicks(); break;
 			}
 		}
 		break;
 
-		case _2_ST_PUNCH_NEUTRAL_JUMP:
+		case ST_PUNCH_NEUTRAL_JUMP:
 		{
 			switch (last_input)
 			{
-			case _2_IN_JUMP_FINISH: state = _2_ST_IDLE; break;
-			case _2_IN_PUNCH_FINISH: state = _2_ST_JUMP_NEUTRAL; break;
+			case IN_JUMP_FINISH: state = ST_IDLE; break;
+			case IN_PUNCH_FINISH: state = ST_JUMP_NEUTRAL; break;
 			}
 		}
 		break;
 
-		case _2_ST_PUNCH_FORWARD_JUMP:
+		case ST_PUNCH_FORWARD_JUMP:
 		{
 			switch (last_input)
 			{
-			case _2_IN_JUMP_FINISH: state = _2_ST_IDLE; break;
-			case _2_IN_PUNCH_FINISH: state = _2_ST_JUMP_FORWARD; break;
+			case IN_JUMP_FINISH: state = ST_IDLE; break;
+			case IN_PUNCH_FINISH: state = ST_JUMP_FORWARD; break;
 			}
 		}
 		break;
 
-		case _2_ST_PUNCH_BACKWARD_JUMP:
+		case ST_PUNCH_BACKWARD_JUMP:
 		{
 			switch (last_input)
 			{
-			case _2_IN_JUMP_FINISH: state = _2_ST_IDLE; break;
-			case _2_IN_PUNCH_FINISH: state = _2_ST_JUMP_BACKWARD; break;
+			case IN_JUMP_FINISH: state = ST_IDLE; break;
+			case IN_PUNCH_FINISH: state = ST_JUMP_BACKWARD; break;
 			}
 		}
 		break;
 
-		case _2_ST_PUNCH_STANDING_L:
+		case ST_PUNCH_STANDING_L:
 		{
 			switch (last_input)
 			{
-			case _2_IN_PUNCH_FINISH: state = _2_ST_IDLE; break;
+			case IN_PUNCH_FINISH: c_punch1->to_delete = true;  state = ST_IDLE;  break;
 			}
 		}
 		break;
 
-		case _2_ST_CROUCH:
+		case ST_CROUCH:
 		{
 			switch (last_input)
 			{
-			case _2_IN_X: state = _2_ST_PUNCH_CROUCH; punch_timer = SDL_GetTicks(); break;
-			case _2_IN_CROUCH_UP: state = _2_ST_IDLE; break;
+			case IN_X: state = ST_PUNCH_CROUCH; punch_timer = SDL_GetTicks(); break;
+			case IN_CROUCH_UP: state = ST_IDLE; break;
 
 			}
 		}
 		break;
-		case _2_ST_PUNCH_CROUCH:
+		case ST_PUNCH_CROUCH:
 		{
 			switch (last_input)
 			{
 
-			case _2_IN_PUNCH_FINISH: state = _2_ST_IDLE; break;
+			case IN_PUNCH_FINISH: state = ST_IDLE; break;
 			}
 		}
 		break;
@@ -364,6 +376,7 @@ p2_states ModulePlayer2::process_fsm(p2Qeue<p2_inputs>& inputs)
 
 	return state;
 }
+
 
 	
 
@@ -380,78 +393,77 @@ update_status ModulePlayer2::Update()
 
 	collider->SetPos(position.x - 30, position.y - 90);
 
-	current_state = _2_ST_UNKNOWN;
-	//printf("Listening for WASD + SPACE:\n");
-
+	current_state = ST_UNKNOWN;
+	
 	if (external_input(inputs))
 	{
 		internal_input(inputs);
 
-		p2_states state = process_fsm(inputs);
+		p1_states state = process_fsm(inputs);
 
 		if (state != current_state)
 		{
 			switch (state)
 			{
-			case _2_ST_IDLE:
-				current_animation = &idle;
+			case ST_IDLE:
+				//	std::cout<<"IDLE\n";
 				break;
-			case _2_ST_HIT:
-			
+			case ST_HIT:
+				//	std::cout << "HIT\n";
 				break;
-			case _2_ST_WALK_FORWARD:
-				if (position.x < 896.0 && position.x < (App->renderer->OpCamera.x) + SCREEN_WIDTH)
+
+			case ST_WALK_FORWARD:{
+				//	std::cout << "WALK FORWARD >>>>\n";
+				if (App->player2->position.x < 860.0 && App->player2->position.x < (App->renderer->OpCamera.x) + SCREEN_WIDTH)
 				{
 					current_animation = &forward;
-					position.x += speed;
-
+					App->player2->position.x += speed;
 					collider->SetPos(position.x - 30, position.y - 90);
-
-					//App->player2_col->AddCollider(detection, COLLIDER_NEUTRAL_2, NULL);
-
 				}
-				break;
-			case _2_ST_WALK_BACKWARD:
-				if (position.x > 0.0 && position.x > (App->renderer->OpCamera.x) + 10)
+			}
+								 break;
+
+			case ST_WALK_BACKWARD:
+				//	std::cout << "WALK BACKWARD <<<<\n";
+			{
+				if (App->player->position.x > 0.0 && App->player->position.x > (App->renderer->OpCamera.x) + 20)
 				{
-					current_animation = &forward;
+					current_animation = &backward;
 					position.x -= speed;
 					collider->SetPos(position.x - 30, position.y - 90);
 				}
+			}
+			break;
+			case ST_JUMP_NEUTRAL:
+				//std::cout << "JUMPING NEUTRAL ^^^^\n";
 				break;
-			case _2_ST_JUMP_NEUTRAL:
-			
+			case ST_JUMP_FORWARD:
+				//std::cout << "JUMPING FORWARD ^^>>\n";
 				break;
-			case _2_ST_JUMP_FORWARD:
-	
+			case ST_JUMP_BACKWARD:
+				//std::cout << "JUMPING BACKWARD ^^<<\n";
 				break;
-			case _2_ST_JUMP_BACKWARD:
-			
+			case ST_CROUCH:
+				//std::cout << "CROUCHING ****\n";
 				break;
-			case _2_ST_CROUCH:
-				
+			case ST_PUNCH_CROUCH:
+				//std::cout << "PUNCH CROUCHING **++\n";
 				break;
-			case _2_ST_PUNCH_CROUCH:
-		
-				break;
-			case _2_ST_PUNCH_STANDING_L:
+			case ST_PUNCH_STANDING_L:
+				//std::cout << "PUNCH STANDING ++++\n";
 				current_animation = &punch;
-				if (isOnLeft){
+				collider->SetPos(position.x + 10, position.y - 75);
 
-					c_punch1 = App->colision->AddCollider({ position.x + 10, position.y - 75, 60, 10 }, COLLIDER_PUNCH_2, this);
-				}
-				else{
-
-					c_punch1 = App->colision->AddCollider({ position.x - 60, position.y - 75, 60, 10 }, COLLIDER_PUNCH_2, this);
-				}
-				break;
-			case _2_ST_PUNCH_NEUTRAL_JUMP:
 
 				break;
-			case _2_ST_PUNCH_FORWARD_JUMP:
+			case ST_PUNCH_NEUTRAL_JUMP:
+				//std::cout << "PUNCH JUMP NEUTRAL ^^++\n";
 				break;
-			case _2_ST_PUNCH_BACKWARD_JUMP:
-
+			case ST_PUNCH_FORWARD_JUMP:
+				//std::cout << "PUNCH JUMP FORWARD ^>>+\n";
+				break;
+			case ST_PUNCH_BACKWARD_JUMP:
+				//std::cout << "PUNCH JUMP BACKWARD ^<<+\n";
 				break;
 			}
 		}
--- ModulePlayer2.h
+++ ModulePlayer2.h
@@ -3,6 +3,7 @@
 #include "Animation.h"
 #include "Globals.h"
 #include "p2Point.h"
+#include "p2Qeue.h"
 #include "ModuleCollision.h"
 #include "States.h"
 class ModulePlayer2 : public Module
@@ -14,6 +15,10 @@ public:
 	bool Start();
 	update_status Update();
 	bool CleanUp();
+
+	//State Machine variables
+	p2Qeue<p1_inputs> inputs;
+	p1_states current_state;
 
 	void OnCollision(Collider* c1, Collider* c2);
 public:
@@ -28,23 +33,27 @@ public:
 	SDL_Rect atac;
 	SDL_Rect defense;
 	SDL_Rect detection;
+	//Pointer to current animation
+	Animation* current_animation;
+
 	//Colliders
 	Collider* collider;
 	Collider* c_punch1;
 	Collider* c_punch2;
 	Collider* c_kick;
 
-	//State Machine variables
-	p2Qeue<p2_inputs> inputs;
-	p2_states current_state;
+	
 	//State Machine Methods
-	bool external_input(p2Qeue<p2_inputs>& inputs);
-	void internal_input(p2Qeue<p2_inputs>& inputs);
-	p2_states process_fsm(p2Qeue<p2_inputs>& inputs);
+	bool external_input(p2Qeue<p1_inputs>& inputs);
+	void internal_input(p2Qeue<p1_inputs>& inputs);
+	p1_states process_fsm(p2Qeue<p1_inputs>& inputs);
+	
 	//Timers
 	Uint32 jump_timer;
 	Uint32 punch_timer;
 	Uint32 hit_timer;
+
+
 	//Not so evil bools
 	bool isOnLeft;
 	bool isAttacking;
