5914e7174403e699e4153ef210f0a08c11aaaaaf
Ian <itrueba0809@gmail.com>
2015-06-12 05:04:13 +0200

    Player2 Start

--- ModulePlayer2.h
+++ ModulePlayer2.h
@@ -35,6 +35,10 @@ public:
 	Animation backward;
 	Animation jump;
 	Animation jumpfalling;
+	Animation jumpforward;
+	Animation jumpforwardfalling;
+	Animation jumpbackward;
+	Animation jumpbackwardfalling;
 	Animation block;
 	Animation crouch;
 	Animation crouchidle;
@@ -42,9 +46,15 @@ public:
 	Animation punch2;
 	Animation kick;
 	Animation kick2;
+	Animation crouchpunch;
+	Animation crouchkick;
+	Animation jumppunch;
+	Animation jumpkick;
+	Animation hadoukenmove;
 	unsigned int normalFX;
 	unsigned int strongFX;
 	unsigned int fallingFX;
+	unsigned int hadoukenFX;
 
 	//State Machine variables
 	p2Qeue<p1_inputs> inputs2;
@@ -55,19 +65,34 @@ public:
 
 	bool isCrouching;
 	bool isJumping;
+	bool isJumpingF;
+	bool isJumpingB;
 	bool isFalling;
+	bool isGoingRight;
+	bool isGoingLeft;
 	bool isAttacking;
 	bool isPunching_L;
 	bool isPunching_H;
+	bool isPunching_Crouch;
 	bool isKicking_L;
 	bool isKicking_H;
+	bool isJumpPunching;
+	bool isCrouchPunching;
+	bool isJumpKicking;
+	bool isCrouchKicking;
+	bool isAttackHard;
 
 	bool isHit;
+
 	bool doDefense;
 	//PNJ basics
 	p2Point<int> position;
 	unsigned int Health;
 	float speed;
+	long hadouken_timer;
+	int or_pos_hadouken;
+	long sp_timer;
+	int sp_check;
 
 	//SDL_Rect detection;
 	Collider* collider;
@@ -76,6 +101,7 @@ public:
 	Collider* c_punch2;
 	Collider* c_kick;
 	Collider* c_kick2;
+	Collider* c_hadouken;
 
 
 	
